8391c0d6a039adf3359716ac20aa3a4251a52be5
wael2000 <waelibrahim2000@hotmail.com>
2017-01-10 13:43:44 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('server.crt')
+};
+console.log('key');
+console.log('crt');
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+}).listen(8443);
